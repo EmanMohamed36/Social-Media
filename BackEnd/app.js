6c0ae404a13a3f12e2fbfccf1de2ec0489400cdc
EmanMohamed36 <ei3377898@gmail.com>
2023-11-22 18:40:34 +0200

    Adding Remote Data Base

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -2,15 +2,18 @@ import express from "express";
 import mongoose from "mongoose";
 import router from "./routes/user.js";
 import blogRouter from "./routes/blog-routes.js";
+import dotenv from "dotenv" 
+dotenv.config()
 
 const app = express();
 app.use(express.json())
 app.use("/api/user", router)
 app.use("/api/blog",blogRouter)
 mongoose
-    .connect("mongodb://localhost:27017")
-    .then(() => app.listen(3000))
+    .connect(process.env.MongoDbUrl)
+    .then(() => app.listen(process.env.PORT))
     .then(() => console.log("connect to mongoDb and listen"))
     .catch((err) => console.log(err))
 
     //sudo systemctl start mongod
+    
